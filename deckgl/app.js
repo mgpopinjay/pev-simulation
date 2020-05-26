3f9449dfb30913ec5ff88382e691c2fa5e8b42b7
suverino <suverino.frith@gmail.com>
2020-05-25 22:09:24 -0400

    install webpac

--- deckgl/app.js
+++ deckgl/app.js
@@ -1,6 +1,23 @@
 import {Deck} from '@deck.gl/core';
 import {TripsLayer} from '@deck.gl/geo-layers';
 
-const App = ({data, viewport}) => {
-	
+const DATA_URL = {
+	TRIPS: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/trips/trips-v7.json'
 };
+
+const INITIAL_VIEW_STATE = {
+	longitude: -74,
+	latitute: 40.72,
+	zoom: 13,
+	pitch 45,
+	bearing: 0
+};
+
+const deckgl = new Deck({
+	initialViewState: INITIAL_VIEW_STATE,
+	layers: [
+		new TripsLayers({
+			data: DATA_URL,
+		})
+	]
+});
