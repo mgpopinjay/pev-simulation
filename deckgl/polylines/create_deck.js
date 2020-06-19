f1de702e062045f97db6106f51e4780205b1a8ab
suverino <suverino.frith@gmail.com>
2020-06-18 20:16:26 -0400

    display multiple pev trips on iconlayer

--- deckgl/polylines/create_deck.js
+++ deckgl/polylines/create_deck.js
@@ -6,24 +6,32 @@ let sim_results = require('./sim_results_3546.json');
 
 let trips = sim_results["fleet"]["0"]["history"];
 
-console.log(trips);
-/*
+//console.log(trips);
+
+
 let routes = [];
 let route;
+let trip = [];
+let overview = [];
 
-for (route of ) {
-	let overview = [];
-	let point;
+for (route of trips) {
+	if ("overview_polyline" in route && "steps_polyline" in route) {
+		let trip = polyline.decode(route["overview_polyline"]);
 
-	for (point of route) {
-		console.log(point);
-		overview.push({"coordinates":point.reverse()});
+		//let overview = [];
+		let point;
+		//console.log(trip);
+		for (point of trip) {
+			//console.log(point);
+			overview.push({"coordinates":point.reverse()});
+			//console.log(overview);
+		}
+
+		routes.push(overview);
 	}
-	
 }
 
 console.log(routes);
 let data = JSON.stringify(overview);
 console.log(data);
 fs.writeFileSync('overview.json', data);
-*/
