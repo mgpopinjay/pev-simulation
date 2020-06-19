36e3e2126ca74ee41b2afad9e02cc0ebe795da4f
suverino <suverino.frith@gmail.com>
2020-06-19 10:16:48 -0400

    display all pev trips on IconLayer

--- deckgl/polylines/create_deck.js
+++ deckgl/polylines/create_deck.js
@@ -4,34 +4,38 @@ const fs = require('fs');
 let polyline = require("./polyline.js");
 let sim_results = require('./sim_results_3546.json');
 
-let trips = sim_results["fleet"]["0"]["history"];
+let trips = sim_results["fleet"];
 
 //console.log(trips);
 
 
 let routes = [];
-let route;
-let trip = [];
-let overview = [];
+let overview = []; // holds all coordinates from every trip
 
-for (route of trips) {
-	if ("overview_polyline" in route && "steps_polyline" in route) {
-		let trip = polyline.decode(route["overview_polyline"]);
-
-		//let overview = [];
-		let point;
-		//console.log(trip);
-		for (point of trip) {
-			//console.log(point);
-			overview.push({"coordinates":point.reverse()});
-			//console.log(overview);
+for (const [key, value] of Object.entries(trips)) {
+	// console.log(key, value);
+	let route;
+	let trip = [];
+	let pev = value["history"];
+	for (route of pev) {
+		// console.log(route);
+		if ("overview_polyline" in route && "steps_polyline" in route) {
+			let trip = polyline.decode(route["overview_polyline"]);
+			
+			//let overview = [];
+			let point;
+			for (point of trip) {
+				//console.log(point);
+				overview.push({"coordinates":point.reverse()});
+			}
+			
+			routes.push(overview);
 		}
-
-		routes.push(overview);
 	}
 }
 
-console.log(routes);
+// console.log(routes);
+console.log(overview);
 let data = JSON.stringify(overview);
 console.log(data);
 fs.writeFileSync('overview.json', data);
