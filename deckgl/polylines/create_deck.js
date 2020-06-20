7fcf4e14dd49abcc1006c87e28426168dd04f6d3
suverino <suverino.frith@gmail.com>
2020-06-20 15:47:19 -0400

    plot step_polyline and overview_polyline together

--- deckgl/polylines/create_deck.js
+++ deckgl/polylines/create_deck.js
@@ -11,6 +11,9 @@ let trips = sim_results["fleet"];
 
 let routes = [];
 let overview = []; // holds all coordinates from every trip
+for(var i = 0; i < 207; i++) { // remove most trips
+	delete trips["" + i];
+}
 
 for (const [key, value] of Object.entries(trips)) {
 	// console.log(key, value);
@@ -23,14 +26,23 @@ for (const [key, value] of Object.entries(trips)) {
 			//let trip = polyline.decode(route["overview_polyline"]);
 			trip = [];
 			for (const t of route["steps_polyline"]) {
-				trip.push(polyline.decode(t));
-				console.log(t);
+				let r = polyline.decode(t);
+				for (const s of r) {
+					console.log(s);
+					trip.push(s);
+				}
 			}
+			let trip2 = polyline.decode(route["overview_polyline"])
 			
 			//let overview = [];
 			let point;
 			let color = [Math.floor((Math.random() * 255) + 1), Math.floor((Math.random() * 255) + 1), Math.floor((Math.random() * 255) + 1)];
 			for (point of trip) {
+				console.log("Point:" + point);
+				overview.push({"coordinates":point.reverse(), "color":color});
+			}
+			color = [Math.floor((Math.random() * 255) + 1), Math.floor((Math.random() * 255) + 1), Math.floor((Math.random() * 255) + 1)];
+			for (point of trip2) {
 				console.log("Point:" + point);
 				overview.push({"coordinates":point.reverse(), "color":color});
 			}
@@ -40,8 +52,8 @@ for (const [key, value] of Object.entries(trips)) {
 	}
 }
 
-// // console.log(routes);
-// console.log(overview);
-// let data = JSON.stringify(overview);
-// console.log(data);
-// fs.writeFileSync('overview.json', data);
+console.log(routes);
+console.log(overview);
+let data = JSON.stringify(overview);
+console.log(data);
+fs.writeFileSync('overview.json', data);
